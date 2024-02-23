b26ed34e01038232821b12d83f458b928cc73ade
Alexander Monzon <mmonzonp4@miumg.edu.gt>
2024-02-22 21:56:32 -0600

    Evalua si es numero entero, alteración del codigo.

--- Ejercicio12/Program.cs
+++ Ejercicio12/Program.cs
@@ -8,26 +8,36 @@ Thread.Sleep(2000);
 Console.WriteLine();
 
 Console.Write("Ingrese la calificación del examen: ");
-int calificacion = Convert.ToInt32(Console.ReadLine());
+string input = Console.ReadLine();
 
-switch (calificacion)
+if (int.TryParse(input, out int calificacion))
 {
-    case int n when (n >= 95 && n <= 100):
-        Console.WriteLine("Cerebrito mi hermano");
-        break;
-    case int n when (n >= 80 && n <= 89):
-        Console.WriteLine("Notable");
-        break;
-    case int n when (n >= 70 && n <= 79):
-        Console.WriteLine("Aprobatoria");
-        break;
-    case int n when (n >= 60 && n <= 69):
-        Console.WriteLine("No aprobatoria");
-        break;
-    case int n when (n >= 45 && n <= 68):
-        Console.WriteLine("REPITE EL CURSO");
-        break;
-    default:
-        Console.WriteLine("Calificación no válida");
-        break;
+
+    switch (calificacion)
+    {
+        case int n when (n >= 95 && n <= 100):
+            Console.WriteLine("Cerebrito mi hermano");
+            break;
+        case int n when (n >= 80 && n <= 89):
+            Console.WriteLine("Notable");
+            break;
+        case int n when (n >= 70 && n <= 79):
+            Console.WriteLine("Aprobatoria");
+            break;
+        case int n when (n >= 60 && n <= 69):
+            Console.WriteLine("No aprobatoria");
+            break;
+        case int n when (n >= 45 && n <= 68):
+            Console.WriteLine("REPITE EL CURSO");
+            break;
+        default:
+            Console.WriteLine("Calificación no válida");
+            break;
+    }
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("La calificación debe ser un número entero");
+    Console.ResetColor();
 }
